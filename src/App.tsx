ceb94164e9d08b295b4bdaa353cc75c5e0687c83
Krushna Dash <52315347+Klutch42@users.noreply.github.com>
2025-10-10 03:39:35 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+<About />
 import Projects from "./pages/Projects";
 import Notes from "./pages/Notes";
 import NotFound from "./pages/NotFound";
